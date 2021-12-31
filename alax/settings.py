cda4851900f52916204023937a5a17f9143303a8
Ravi Raushan <85803902+raushan-sen@users.noreply.github.com>
2021-12-31 21:03:30 +0530

    Update settings.py

--- alax/settings.py
+++ alax/settings.py
@@ -109,9 +109,9 @@ STATIC_URL = 'static/'
 #STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = BASE_DIR / 'static'
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
