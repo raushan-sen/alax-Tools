4b605a1ea208d350cce37495c5ff05f7df144337
Ravi Raushan <85803902+raushan-sen@users.noreply.github.com>
2021-12-31 20:30:53 +0530

    Update settings.py

--- alax/settings.py
+++ alax/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a5t3e@mozgj8+^!kfl#0a7f_iw4)9@tq@t9x0i$5#x@@t2#62!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://alaxttt.herokuapp.com']
